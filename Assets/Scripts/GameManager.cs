e9e3f6f758e10494d91901b50073235a8564a0aa
CoderJae777 <gohyishen777@gmail.com>
2025-10-14 21:59:50 +0800

    [BUG] Restart button works but only once

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     public int score { get; private set; }
     public bool IsPlaying { get; private set; }
+    public TMPro.TMP_Text scoreText;
 
     void Start()
     {
         StartCoroutine(GameLoop());
+        UpdateScoreUI();
     }
 
-    public void AddOnePoint() => score++;
-    public void ResetScore() => score = 0;
+    public void OnRestartButtonPressed()
+    {
+        ResetScore();
+        OnPlayerDied();
+        resetPlayerPosition();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void resetPlayerPosition()
+    {
+        var player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = Vector3.zero;
+            var rb = player.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+    }
+
+    public void AddOnePoint()
+    {
+        score++;
+        UpdateScoreUI();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreUI();
+    }
+
+    public void UpdateScoreUI()
+    {
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
+    }
 
     public void OnPlayerDied()
     {
@@ -55,5 +95,30 @@ public class GameManager : Singleton<GameManager>
     public void ForceStopGame()
     {
         IsPlaying = false;
+    }
+
+    // --- For Unity's scene manager --- //
+    // Because this is a singleton, it will persist across scenes.
+    // We need to reset the game state when a new scene is loaded.
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // we need this ebcause idk why the game manager is not finding the
+        // score text in the new scene on restart
+        var go = GameObject.Find("ScoreText");
+        if (go != null)
+        {
+            scoreText = go.GetComponent<TMPro.TMP_Text>();
+            UpdateScoreUI();
+        }
     }
 }
